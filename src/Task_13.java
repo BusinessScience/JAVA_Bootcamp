ebbda9fe70d4e8cdfb0b68662f39b51348268f42
Business Science <208981291+BusinessScience@users.noreply.github.com>
2025-04-24 23:59:49 +0200

    She is back

---
+++ src/Task_13.java
@@ -0,0 +1,12 @@
+public class Task_13 {
+    public static void main(String[] args) {
+
+        String s = "Stifler's Mom";
+        System.out.println(s);
+        System.out.println(s.length());
+        System.out.println(s.charAt(0));
+
+        String roughLife = s + " and Me";
+        System.out.println(roughLife);
+    }
+}
