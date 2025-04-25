ddee769379d9deb44bb626eb1e89f0b6c1bbbbba
Business Science <208981291+BusinessScience@users.noreply.github.com>
2025-04-25 09:16:05 +0200

    Task15

---
+++ src/Taska_15.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Taska_15 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj imię: ");
+        String name = scanner.next();
+
+        if (name.endsWith("a") && !name.equals("Barnaba")) System.out.println("To imię żeńskie");
+        else System.out.println("Imię męskie");
+    }
+}
