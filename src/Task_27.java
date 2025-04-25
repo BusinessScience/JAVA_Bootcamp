25c7072346398b8ddfbc6ae67a9cf33d9663ea34
Business Science <208981291+BusinessScience@users.noreply.github.com>
2025-04-25 20:26:06 +0200

    Task_27/ update- add funcion 'two table sum'

--- src/Task_27.java
+++ src/Task_27.java
@@ -35,6 +35,22 @@ public class Task_27 {
         return numbers[numbers.length/2];
     }
 
+    private static int[] newTab(int[] tab1, int[] tab2){
+        int[] temp = new int[tab1.length + tab2.length];
+        for (int i = 0; i < temp.length; i++) {
+            if (i < tab1.length) temp[i] = tab1[i];
+            else temp[i] = tab2[i-tab1.length];
+        }
+        return temp;
+    }
+
+    private static void soutTab(int[] tab){
+
+        for (int i = 0; i < tab.length; i++) {
+            System.out.print(tab[i] + " | ");
+        }
+    }
+
     public static void main(String[] args) {
 
         int[] numbers = {3,4,5,3,2};
@@ -47,5 +63,7 @@ public class Task_27 {
         System.out.println(sumElements2(numbers) + sumElements2(numbers2));
         System.out.println(sumFirstLast(numbers));
         System.out.println("Element środkowy: " + elemMiddle( numbers));
+        
+        soutTab(newTab(numbers2, numbers));
     }
 }
