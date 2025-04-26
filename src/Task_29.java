2b3cd6ac8070f263d4bb42fd57bc6b55c68145ab
Business Science <208981291+BusinessScience@users.noreply.github.com>
2025-04-26 11:28:03 +0200

    Task_29

---
+++ src/Task_29.java
@@ -0,0 +1,33 @@
+public class Task_29 {
+
+    private static String[] getOnlyLettersLong(String[] words, int howLong) {
+        int count = 0;
+
+        for (String s : words) {
+            if (s.length() == howLong)
+                count++;
+        }
+
+        String[] temp = new String[count];
+
+        int j = 0;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() == howLong) {
+                temp[j] = words[i];
+                j++;
+            }
+        }
+        return temp;
+    }
+
+    private static void soutTab(String[] tab) {
+        for (String i : tab) System.out.print(i + " | ");
+    }
+
+    public static void main(String[] args) {
+
+        String[] words = new String[]{"12345", "123", "21", "54321", "132", "12", "13245", "123", "22", "1", "123456"};
+
+        soutTab(getOnlyLettersLong(words, 4));
+    }
+}
