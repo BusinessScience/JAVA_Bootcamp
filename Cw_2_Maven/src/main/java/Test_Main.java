977188da755299cf10ce78a4ec2fd8b4ebb69071
Business Science <208981291+BusinessScience@users.noreply.github.com>
2025-04-28 13:15:49 +0200

    clean up

--- Cw_2_Maven/src/main/java/Test_Main.java
+++ Cw_2_Maven/src/main/java/Test_Main.java
@@ -1,9 +1,8 @@
 import com.google.common.collect.Lists;
 
-import java.awt.*;
 import java.util.ArrayList;
 
-public class Main {
+public class Test_Main {
     public static void main(String[] args) {
 
         ArrayList<Integer> guavaList = Lists.newArrayList(1,2,3,2,1,2,3,4,5,6,453,2,2);
