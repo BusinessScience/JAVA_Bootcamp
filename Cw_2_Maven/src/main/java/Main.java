56d46dc6226642239a4288ab01bd7fbd785c15b7
Business Science <208981291+BusinessScience@users.noreply.github.com>
2025-04-28 10:30:21 +0200

    Now it works :D

--- Cw_2_Maven/src/main/java/Main.java
+++ Cw_2_Maven/src/main/java/Main.java
@@ -1,5 +1,13 @@
+import com.google.common.collect.Lists;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
+        ArrayList<Integer> guavaList = Lists.newArrayList(1,2,3,2,1,2,3,4,5,6,453,2,2);
+        guavaList.sort(Integer::compareTo);
+        guavaList.forEach(e -> System.out.print(e + " | "));
     }
 }
